94a22ad0af1390a38cb17689d7b7a9bf77a6e9fb
masamasa0717 <masamasa0717@icloud.com>
2024-06-17 10:30:26 +0900

    Main.javaの修正

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster!");
-    Monster monster = new Monster();
-    System.out.println(monster.toString());
+    //Monster monster = new Monster();
+    //System.out.println(player.toString());
+    
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
